08e54ea6ec8f74f847f89d169e9852935ba0ce62
giwan <giwan.dev@gmail.com>
2023-02-26 23:52:55 +0900

    ✨ Spotify 관련 필수 환경 변수를 .env에서 가져오는 모듈 추가

---
+++ common/env.ts
@@ -0,0 +1,11 @@
+export const SPOTIFY_CLIENT_ID = getEnvironmentVariable("SPOTIFY_CLIENT_ID");
+export const SPOTIFY_CLIENT_SECRET =getEnvironmentVariable("SPOTIFY_CLIENT_SECRET");
+
+function getEnvironmentVariable(key: string): string {
+  const value = process.env[key];
+
+  if (value === undefined) {
+    throw new Error(`.env 파일에 명시하지 않은 변수: ${key}`);
+  }
+  return value;
+}
