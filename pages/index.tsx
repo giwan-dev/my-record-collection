87280140afa0e30fba07c15e63a507a80d3143ce
giwan <giwan.dev@gmail.com>
2023-03-01 00:52:36 +0900

    ✨ 홈에서 앨범 검색 페이지로 이동하는 링크 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,8 @@ export default function Home() {
       {user === undefined && <Link href="/login">Spotify 로그인</Link>}
 
       {user !== undefined && <h1>안녕하세요, {user.display_name}</h1>}
+
+      <Link href="/search-album">스포티파이에서 앨범 검색하기</Link>
     </main>
   );
 }
