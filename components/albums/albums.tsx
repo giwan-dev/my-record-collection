6e645ea6cc82d4e730166835368b97b7491d7286
giwan <giwan.dev@gmail.com>
2023-03-01 12:04:20 +0900

    ✨ 앨범 목록 뷰 간단 구현

---
+++ components/albums/albums.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+
+interface Album {
+  id: string;
+  title: string;
+  artist: string;
+  imageUrl: string | null;
+}
+
+const albums: Album[] = [
+  {
+    id: "1",
+    title: "Bleached",
+    artist: "Loco",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d0000b27311ffae18db3eeb12217bf206",
+  },
+  {
+    id: "2",
+    title: "Original",
+    artist: "차붐",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d0000b2734fdf206be2d70b5fee489c79",
+  },
+];
+
+export function Albums() {
+  return (
+    <ul className="flex flex-wrap gap-8 p-10">
+      {albums.map((album) => (
+        <li key={album.id} className="flex flex-col gap-y-2">
+          {album.imageUrl ? (
+            <Image
+              src={album.imageUrl}
+              width={320}
+              height={320}
+              alt={`${album.title} 썸네일`}
+            />
+          ) : null}
+
+          <div className="flex gap-x-2">
+            <span className="text-neutral-800 text-lg">{album.artist}</span>
+            <span className="text-neutral-800 font-bold text-xl">
+              {album.title}
+            </span>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 
+import { Albums } from "@/components/albums";
 import { useSpotifyUser } from "@/components/spotify-user";
 
 export default function Home() {
   const user = useSpotifyUser();
+
   return (
     <main>
+      <Albums />
+
       {user === undefined && <Link href="/login">Spotify 로그인</Link>}
 
       {user !== undefined && <h1>안녕하세요, {user.display_name}</h1>}
-
-      <Link href="/search-album">스포티파이에서 앨범 검색하기</Link>
     </main>
   );
 }
