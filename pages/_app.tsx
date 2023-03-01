0c637bd954e9f8bcef1f1f03dfda7530b6c620b7
giwan <giwan.dev@gmail.com>
2023-03-01 18:27:45 +0900

    🛂 인증 관련 정보를 클라이언트에서 사용할 수 있도록 공급

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 
 import "@/common/global.css";
 import { SpotifyUserProvider } from "@/components/spotify-user";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session | null }>) {
   return (
-    <SpotifyUserProvider>
-      <Component {...pageProps} />
-    </SpotifyUserProvider>
+    <SessionProvider session={session}>
+      <SpotifyUserProvider>
+        <Component {...pageProps} />
+      </SpotifyUserProvider>
+    </SessionProvider>
   );
 }
