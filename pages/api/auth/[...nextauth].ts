281ba4331ee6b139ed5485a491e0d89442640ff1
giwan <giwan.dev@gmail.com>
2023-03-01 19:02:08 +0900

    🛂 Spotify의 access token을 session에서 사용할 수 있도록 주입

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { NextAuthOptions } from "next-auth";
+import type { NextAuthOptions, Session } from "next-auth";
+import type { JWT } from "next-auth/jwt";
 import NextAuth from "next-auth/next";
 import SpotifyProvider from "next-auth/providers/spotify";
 
@@ -9,10 +10,10 @@ import prisma from "@/services/prisma";
 
 export default function authHandler(req: NextApiRequest, res: NextApiResponse) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return NextAuth(req, res, options);
+  return NextAuth(req, res, nextAuthOptions);
 }
 
-const options: NextAuthOptions = {
+export const nextAuthOptions: NextAuthOptions = {
   providers: [
     SpotifyProvider({
       clientId: SPOTIFY_CLIENT_ID,
@@ -21,4 +22,28 @@ const options: NextAuthOptions = {
   ],
   adapter: PrismaAdapter(prisma),
   secret: SECRET,
+  callbacks: {
+    jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    session({ session, token }: { session: Session; token: JWT }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+  }
+}
