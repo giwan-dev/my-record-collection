e0e2d02bcfd7baa33cb46ec67fb90bc20ef86a16
giwan <giwan.dev@gmail.com>
2023-03-01 22:39:48 +0900

    💄 앨범 목록 스타일 조정

--- components/albums/albums.tsx
+++ components/albums/albums.tsx
@@ -2,15 +2,12 @@ import type { Album } from "@prisma/client";
 import Image from "next/image";
 
 export function Albums({ albums }: { albums: Album[] }) {
-  const imageSize = 240;
+  const imageSize = 300;
 
   return (
-    <ul className="flex flex-wrap gap-4">
+    <ul className="grid grid-cols-3">
       {albums.map((album) => (
-        <li
-          key={album.id}
-          className="flex-0 px-2 py-4 rounded-lg border flex flex-col gap-y-2 "
-        >
+        <li key={album.id} className="flex flex-col">
           {album.imageUrl ? (
             <Image
               src={album.imageUrl}
@@ -20,10 +17,10 @@ export function Albums({ albums }: { albums: Album[] }) {
             />
           ) : null}
 
-          <div className="flex gap-x-2 text-base text-neutral-700">
-            <span>{album.artist}</span>
+          <span className="text-base text-neutral-700 whitespace-nowrap overflow-hidden text-ellipsis">
+            <span className="inline-block mr-1">{album.artist}</span>
             <span className="font-bold">{album.title}</span>
-          </div>
+          </span>
         </li>
       ))}
     </ul>
