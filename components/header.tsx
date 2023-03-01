6d323aa0ec931b2c86482a219a7996d9c6a4ff19
giwan <giwan.dev@gmail.com>
2023-03-02 06:49:14 +0900

    💄 헤더 디자인 간소화

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,8 @@
-import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Header() {
   return (
-    <nav className="py-2 px-4 shadow-sm sticky top-0 bg-white flex justify-between gap-x-2">
-      <Link href="/" className="text-lg font-light">
-        음반 모음
-      </Link>
-
+    <nav className="py-2 px-4 shadow-sm sticky top-0 bg-white flex justify-end gap-x-2">
       <SessionView />
     </nav>
   );
@@ -18,12 +13,8 @@ function SessionView() {
 
   if (status === "loading") {
     return (
-      <div className="flex justify-end gap-x-1 items-center animate-pulse">
-        <div className="h-3 w-20 bg-neutral-200 rounded-full" />
-
-        <div className="h-8 border px-2 py-1 rounded-lg text-sm flex items-center">
-          <div className="h-3 w-10 bg-neutral-200 rounded-full" />
-        </div>
+      <div className="h-8 border px-2 py-1 rounded-lg text-sm flex items-center animate-pulse">
+        <div className="h-3 w-10 bg-neutral-200 rounded-full" />
       </div>
     );
   }
@@ -42,19 +33,13 @@ function SessionView() {
   }
 
   return (
-    <div className="flex justify-end gap-x-1 items-center">
-      <span>
-        {session.user?.name} ({session.user?.email})
-      </span>
-
-      <button
-        className="h-8 border px-2 py-1 rounded-lg text-sm"
-        onClick={() => {
-          void signOut();
-        }}
-      >
-        로그아웃
-      </button>
-    </div>
+    <button
+      className="h-8 border px-2 py-1 rounded-lg text-sm"
+      onClick={() => {
+        void signOut();
+      }}
+    >
+      로그아웃
+    </button>
   );
 }
