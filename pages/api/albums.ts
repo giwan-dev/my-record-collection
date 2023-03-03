0b5af987741cb1d997b60457c960e5ab31611161
giwan <giwan.dev@gmail.com>
2023-03-04 00:27:01 +0900

    ✨ GET /albums API 추가

--- pages/api/albums.ts
+++ pages/api/albums.ts
@@ -1,4 +1,4 @@
-import type { Album } from "@prisma/client";
+import type { Album, Prisma } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 
@@ -15,7 +15,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== "POST") {
+  if (req.method !== "POST" && req.method !== "GET") {
     res.status(405).send("");
     return;
   }
@@ -27,17 +27,40 @@ export default async function handler(
     return;
   }
 
-  if (typeof req.body !== "string") {
-    res.status(400).json({ error: "Invalid body format" });
-    return;
-  }
-
-  const data = JSON.parse(req.body) as ValuesForCreatingAlbum;
-
   try {
-    await prismaClient.album.create({ data: { ...data, userId } });
-    res.status(200).send("");
+    if (req.method === "POST") {
+      if (typeof req.body !== "string") {
+        res.status(400).json({ error: "Invalid body format" });
+        return;
+      }
+
+      const data = JSON.parse(req.body) as ValuesForCreatingAlbum;
+
+      await prismaClient.album.create({ data: { ...data, userId } });
+      res.status(200).send("");
+      return;
+    }
+
+    const { order } = req.query;
+
+    if (Array.isArray(order)) {
+      res.status(400).json({ error: "Invalid query format" });
+      return;
+    }
+
+    const orderByMap: Record<string, Prisma.AlbumOrderByWithRelationInput> = {
+      updatedDesc: { updatedAt: "desc" },
+    };
+
+    const albums = await prismaClient.album.findMany({
+      where: { userId },
+      orderBy: order ? [orderByMap[order]] : undefined,
+    });
+
+    res.status(200).json(albums);
   } catch (error) {
+    console.error(error);
+
     if (error instanceof Error) {
       res.status(500).json({ error: error.message });
     }
