891846725522094a94dc0dc7a55109549ccb523b
giwan <giwan.dev@gmail.com>
2023-03-04 00:47:47 +0900

    🎨 /api/albums 디렉토리 생성
    /api/albums 하위 라우트를 생성하기 위해 디렉토리로 변경합니다.

--- pages/api/albums/index.ts
+++ pages/api/albums/index.ts
@@ -4,7 +4,7 @@ import { getServerSession } from "next-auth";
 
 import prismaClient from "@/services/prisma";
 
-import { nextAuthOptions } from "./auth/[...nextauth]";
+import { nextAuthOptions } from "../auth/[...nextauth]";
 
 export type ValuesForCreatingAlbum = Pick<
   Album,
