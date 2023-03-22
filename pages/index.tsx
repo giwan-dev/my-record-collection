a58d185803a11ced9845c105ea7413d146efc0c0
giwan <giwan.dev@gmail.com>
2023-03-22 23:17:46 +0900

    ✨ index 페이지에서 전체 사용자 목록 확인할 수 있도록 변경
    전체 사용자 목록은 SSG합니다.
    
    세션이 없으면 로그인 버튼을 표시하는 기능은 유지합니다.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,72 +1,81 @@
-import type { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+import type { GetStaticPropsResult } from "next";
 import Image from "next/image";
-import { getServerSession } from "next-auth";
+import Link from "next/link";
 import { signIn, useSession } from "next-auth/react";
 
-import { AlbumStack } from "@/components/albums";
 import { Main } from "@/components/main";
-
-import type { AlbumSummary } from "./api/albums";
-import { getAlbums } from "./api/albums";
-import { nextAuthOptions } from "./api/auth/[...nextauth]";
+import prismaClient from "@/services/prisma";
 
 interface Props {
-  albums: AlbumSummary[];
+  users: { id: string; name: string }[];
 }
 
 const HEIGHT_RATIO_OF_SPOTIFY_LOGO = 709 / 2362;
 
-export default function Home({ albums }: Props) {
-  const { status } = useSession();
+export default function Home({ users: allUsers }: Props) {
+  const { status, data: currentUser } = useSession();
 
-  if (status === "unauthenticated") {
-    const width = 120;
-    const height = Math.round(HEIGHT_RATIO_OF_SPOTIFY_LOGO * width);
+  const width = 120;
+  const height = Math.round(HEIGHT_RATIO_OF_SPOTIFY_LOGO * width);
 
-    return (
-      <Main className="h-full flex justify-center items-center">
-        <button
-          className="border rounded-lg px-4 py-2 flex items-center gap-x-1"
-          onClick={() => {
-            void signIn("spotify");
-          }}
-        >
-          <Image
-            className="inline-block"
-            src="/Spotify_Logo_RGB_Green.png"
-            alt="Spotify"
-            width={width}
-            height={height}
-          />
-
-          <span className="text-lg text-stone-900">시작하기</span>
-        </button>
-      </Main>
-    );
-  }
-
-  if (status === "loading") {
-    return <Main />;
-  }
+  const users = allUsers.filter((user) => user.id !== currentUser?.userId);
 
   return (
-    <Main>
-      <AlbumStack albums={albums} />
+    <Main className="px-4 py-6 flex flex-col gap-y-4">
+      {status === "unauthenticated" && (
+        <section>
+          <button
+            className="border rounded-lg px-4 py-2 flex items-center gap-x-1 hover:bg-stone-100 active:bg-stone-200 transition-colors"
+            onClick={() => {
+              void signIn("spotify");
+            }}
+          >
+            <Image
+              className="inline-block"
+              src="/Spotify_Logo_RGB_Green.png"
+              alt="Spotify"
+              width={width}
+              height={height}
+            />
+
+            <span className="text-lg text-stone-900">시작하기</span>
+          </button>
+        </section>
+      )}
+
+      <section className="flex flex-col gap-y-2">
+        {status === "authenticated" && (
+          <Link
+            className="text-stone-500 underline"
+            href={`/users/${currentUser.userId}`}
+          >
+            내 컬렉션 보러 가기
+          </Link>
+        )}
+
+        {users.map((user) => (
+          <Link
+            className="text-stone-500 underline"
+            key={user.id}
+            href={`/users/${user.id}`}
+          >
+            {user.name}님의 컬렉션 보러 가기
+          </Link>
+        ))}
+      </section>
     </Main>
   );
 }
 
-export async function getServerSideProps({
-  req,
-  res,
-}: GetServerSidePropsContext): Promise<GetServerSidePropsResult<Props>> {
-  const { userId } = (await getServerSession(req, res, nextAuthOptions)) ?? {};
+export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
+  const users = await prismaClient.user.findMany();
 
-  if (!userId) {
-    return { props: { albums: [] } };
-  }
-
-  const albums = await getAlbums({ userId, order: "createdDesc" });
-
-  return { props: { albums } };
+  return {
+    props: {
+      users: users.map((user) => ({
+        id: user.id,
+        name: user.name ?? "(이름 없음)",
+      })),
+    },
+  };
 }
